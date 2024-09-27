6c5a77049ec0ef5fa87d310d28cdfc642ef8f158
Breakdonw <34593308+Breakdonw@users.noreply.github.com>
2024-09-27 01:44:02 -0400

    fixes spacing and scrolling on add account form

--- src/components/grids/accountgrid.tsx
+++ src/components/grids/accountgrid.tsx
@@ -78,7 +78,7 @@ export default function AccountGrid() {
 
       <>
         <Form {...form}>
-          <form onSubmit={form.handleSubmit(onSubmit)} className=" space-y-8 ">
+          <form onSubmit={form.handleSubmit(onSubmit)} className=" space-y-8  max-h-screen">
             <FormField
               control={form.control}
               name="accountNumber"
@@ -185,7 +185,7 @@ export default function AccountGrid() {
                 </FormItem>
               )}
             />
-            <Button type="submit">Submit</Button>
+            <Button className='mr-8' type="submit">Submit</Button>
 
             <AlertDialog.Cancel asChild>
               <button className="">
@@ -216,14 +216,14 @@ export default function AccountGrid() {
         </AlertDialog.Trigger>
         <AlertDialog.Portal>
           <AlertDialog.Overlay className="bg-black bg-opacity-55 data-[state=open]:animate-overlayShow fixed inset-0" />
-          <AlertDialog.Content className="data-[state=open]:animate-contentShow fixed top-[50%] left-[50%] xl:max-h-[95vh] lg:max-h-[85vh] w-[90vw] md:max-w-[1000px] sm:max-w-[500px] translate-x-[-50%] translate-y-[-50%] rounded-[6px] bg-slate-800 p-[25px] shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,_hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] focus:outline-none">
+          <AlertDialog.Content className="data-[state=open]:animate-contentShow overflow-auto fixed top-[50%] left-[50%] xl:max-h-[95vh] lg:max-h-[85vh] w-[90vw] md:max-w-[1000px] sm:max-w-[500px] translate-x-[-50%] translate-y-[-50%] rounded-[6px] bg-slate-800 p-[25px] shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,_hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] focus:outline-none">
             <AlertDialog.Title className="text-mauve12 m-0 text-[17px] font-medium">
               Add New Account
             </AlertDialog.Title>
             <AlertDialog.Description className="text-mauve11 mt-4 mb-5 text-[15px] leading-normal">
               This creates a new account based on your specification.
             </AlertDialog.Description>
-            <div className='container max-h-screen overflow-y-scroll	'>
+            <div className=''>
               <CreateAccountForm />
 
               <AlertDialog.Action asChild>
